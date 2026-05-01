8ebf71dd93bc23435d0aa10711e8cbac95c78397
Tsaki05 <tsegaye.fontsere@estudiantat.upc.edu>
2026-05-01 19:12:04 +0200

    execution tests 5.1

--- backend/apps/inventari/admin.py
+++ backend/apps/inventari/admin.py
@@ -4,7 +4,5 @@ from .models import Magatzem, Ubicacio, Treballador, Producte, Lot
 admin.site.register(Magatzem)
 admin.site.register(Ubicacio)
 admin.site.register(Treballador)
-admin.site.register(Superior)
-admin.site.register(Mosso)
 admin.site.register(Producte)
 admin.site.register(Lot)
